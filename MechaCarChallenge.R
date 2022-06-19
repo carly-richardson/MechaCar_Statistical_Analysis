1bc150a8d80b235c2ee139c6cb5afcde91d2ec6f
Carly Richardson <cbrich16@gmail.com>
2022-06-19 17:40:55 -0500

    Added summary statistics on supsension coils.

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -5,3 +5,9 @@ mpg_table <- read.csv('MechaCar_mpg.csv',check.names = F,stringsAsFactors = F) #
 lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=mpg_table) #generate multiple linear regression model
 
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=mpg_table)) #generate summary statistics
+
+coil_table <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors = F)
+
+total_summary <- coil_table %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI), .groups = 'keep') #create summary table with multiple columns
+
+lot_summary <- coil_table %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI), .groups = 'keep') #create summary table with multiple columns
